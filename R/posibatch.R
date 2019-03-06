bcfa83a80f63fce4ce418225c3e500ebeb895700
ChuanJ <jiaochuan@sklmg.edu.cn>
2019-03-06 09:40:22 -0500

    Update posibatch.R

--- R/posibatch.R
+++ R/posibatch.R
@@ -37,8 +37,8 @@ posibatches <- function(dat, Sentrix, posi=TRUE, batch=TRUE, par.prior=TRUE, pri
   #chips<-as.numeric(factor(substr(Sentrix$sampleNames, 1, 10))) 
   #positions<-as.numeric(factor(substr(Sentrix$sampleNames, 12, 17))) 
   
-  chips <- as.numeric(sapply(strsplit(Sentrix$sampleNames, "_"), function(x) x[1]))
-  positions <- as.numeric(sapply(strsplit(Sentrix$sampleNames, "_"), function(x) x[2]))
+  chips <- as.numeric(sapply(strsplit(as.character(Sentrix$sampleNames), "_"), function(x) x[1]))
+  positions <- as.numeric(sapply(strsplit(as.character(Sentrix$sampleNames), "_"), function(x) x[][2]))
                                
   if (length(positions)!=length(chips)){
     stop('positions and chips must have the same length')
